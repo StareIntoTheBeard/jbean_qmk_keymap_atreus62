9dad7a2a13ce10de0d59eae301ce7b1480643f73
Justin Bean <justin.r.bean@gmail.com>
2017-08-27 01:09:44 -0600

    fix bug?

--- keymap.c
+++ keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_BSLS,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_TRNS,     KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    TD(TD_BRACES) },
 		{ KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_TRNS,     KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT },
 		{ KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_ENT,      KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC },
-		{ KC_LCTL,  KC_LALT, KC_GRV,  KC_LGUI, TT(_NAV), KC_BSPC, KC_ENT,      KC_SPC,  TT(_SERVER),  KC_MINS, KC_EQL,  LGUI(KC_Z),  LSFT(LGUI(KC_Z)) }
+		{ KC_LCTL,  KC_LALT, KC_GRV,  KC_LGUI, TT(_NAV), KC_BSPC, KC_ENT,      KC_SPC,  LT(_SERVER, KC_NO),  KC_MINS, KC_EQL,  LGUI(KC_Z),  LSFT(LGUI(KC_Z)) }
 	},
 	
 	[_NAV] = { 
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ TD(TD_VOLD_MT),   KC_F12,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,       KC_MS_U,       KC_RBRC,    KC_VOLU    },
 		{ KC_MPLY,          KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_MS_BTN1, KC_MS_L,       KC_MS_D,       KC_MS_R,    TD(TD_NEXT_PV)    },
 		{ KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_MS_BTN2, LGUI(KC_LBRC), LGUI(KC_RBRC), KC_TRNS,    KC_TRNS    },
-		{ KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, MO(_MACROS),KC_TRNS,       KC_TRNS,       TO(_RESET), LALT(LGUI(KC_POWER))   }
+		{ KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, MO(_MACROS) ,KC_TRNS,       KC_TRNS,       TO(_RESET), LALT(LGUI(KC_POWER))   }
 	},
 	
 	[_SERVER] = {
